6052a7289ecf99f2a2a2dcc22a94087e6da05aac
kaleab <alebachewkaleab@gmail.com>
2024-12-12 10:42:11 +0300

    Implement Start method to initiate the stopwatch with event notification

---
+++ stopwatch.cs
@@ -0,0 +1,31 @@
+sing System;
+using System.Threading;
+
+public class Stopwatch
+{
+    public delegate void StopwatchEventHandler(string message);
+    public event StopwatchEventHandler OnStarted;
+    public event StopwatchEventHandler OnStopped;
+    public event StopwatchEventHandler OnReset;
+
+    private int _timeElapsed; // Elapsed time in seconds
+    private bool _isRunning;
+    private Timer _timer;
+
+    public Stopwatch()
+    {
+        _timeElapsed = 0;
+        _isRunning = false;
+    } \ public void Start()
+    {
+        if (!_isRunning)
+        {
+            _isRunning = true;
+            _timer = new Timer(Tick, null, 0, 1000);
+            OnStarted?.Invoke("Stopwatch Started!");
+        }
+        else
+        {
+            Console.WriteLine("Stopwatch is already running.");
+        }
+    }

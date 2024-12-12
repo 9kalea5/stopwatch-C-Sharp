f3cdc3056ede110ce42dae677c30e6f9821eb92c
kaleab <alebachewkaleab@gmail.com>
2024-12-12 10:51:15 +0300

    Add Reset method to restart the stopwatch and trigger reset event

--- obj/Debug/net9.0/stopwatch.AssemblyInfo.cs
+++ obj/Debug/net9.0/stopwatch.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("stopwatch")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09c27cac93a3f27699e9ededd3293b0b612ccfc9")]
 [assembly: System.Reflection.AssemblyProductAttribute("stopwatch")]
 [assembly: System.Reflection.AssemblyTitleAttribute("stopwatch")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- stopwatch.cs
+++ stopwatch.cs
@@ -3,42 +3,42 @@ using System.Threading;
 
 public class Stopwatch
 {
-    public delegate void StopwatchEventHandler(string message);
-    public event StopwatchEventHandler OnStarted;
-    public event StopwatchEventHandler OnStopped;
-    public event StopwatchEventHandler OnReset;
+  public delegate void StopwatchEventHandler(string message);
+  public event StopwatchEventHandler OnStarted;
+  public event StopwatchEventHandler OnStopped;
+  public event StopwatchEventHandler OnReset;
 
-    private int _timeElapsed; // Elapsed time in seconds
-    private bool _isRunning;
-    private Timer _timer;
+  private int _timeElapsed; // Elapsed time in seconds
+  private bool _isRunning;
+  private Timer _timer;
 
-    public Stopwatch()
-    {
-        _timeElapsed = 0;
-        _isRunning = false;
-    } \ public void Start()
-    {
-        if (!_isRunning)
-        {
-            _isRunning = true;
-            _timer = new Timer(Tick, null, 0, 1000);
-            OnStarted?.Invoke("Stopwatch Started!");
-        }
-        else
-        {
-            Console.WriteLine("Stopwatch is already running.");
-        }
-    }
-       public void Stop()
-    {
-        if (_isRunning)
-        {
-            _isRunning = false;
-            _timer.Dispose();
-            OnStopped?.Invoke("Stopwatch Stopped!");
-        }
-        else
-        {
-            Console.WriteLine("Stopwatch is not running.");
-        }
-    }
+  public Stopwatch()
+  {
+    _timeElapsed = 0;
+    _isRunning = false;
+  }\ public void Start()
+  {
+    if (!_isRunning)
+    {
+      _isRunning = true;
+      _timer = new Timer(Tick, null, 0, 1000);
+      OnStarted?.Invoke("Stopwatch Started!");
+    }
+    else
+    {
+      Console.WriteLine("Stopwatch is already running.");
+    }
+  }
+      public void Stop()
+  {
+    if (_isRunning)
+    {
+      _isRunning = false;      _timer.Dispose();
+      OnStopped?.Invoke("Stopwatch Stopped!");
+    }
+    else
+    {
+      Console.WriteLine("Stopwatch is not running.");
+    }
+  }
+}
